117f3d0c3a23287deb3943f8051262c5233dccdd
JaydenGarrick <33009669+JaydenGarrick@users.noreply.github.com>
2018-11-16 11:46:04 -0700

    Add JournalC xCode Project

---
+++ Afternoon Projects/Unit 4 - Obj-C/Day 1/Journal-C-master/JournalC/JournalC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JournalC
+//
+//  Created by Jayden Garrick on 11/16/18.
+//  Copyright © 2018 Jayden Garrick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
