0a2130052e3faa3988e33acc936463a85cefa1a6
fmartin0212 <fmartin0212@gmail.com>
2018-11-16 12:16:47 -0700

    test project in wk6

---
+++ Afternoon Projects/Unit 6 - Auto Layout & Mini Projects/Day 1/TestProject/TestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Frank Martin Jr on 11/16/18.
+//  Copyright © 2018 DevMtnStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
