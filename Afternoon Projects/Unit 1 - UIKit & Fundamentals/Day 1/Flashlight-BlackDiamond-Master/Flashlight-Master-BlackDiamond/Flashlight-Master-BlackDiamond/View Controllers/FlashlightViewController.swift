4a8508adfae26f95077df74ca3797fa5108d9851
Karl Pfister <pfister@Karls-MacBook-Pro.local>
2019-01-18 17:02:25 -0700

    Added Black Diamonds
    • Added Status Bar logic
    • Added Torch Logic
    • Added Swipe Gesture Logic

--- Afternoon Projects/Unit 1 - UIKit & Fundamentals/Day 1/Flashlight-BlackDiamond-Master/Flashlight-Master-BlackDiamond/Flashlight-Master-BlackDiamond/View Controllers/FlashlightViewController.swift
+++ Afternoon Projects/Unit 1 - UIKit & Fundamentals/Day 1/Flashlight-BlackDiamond-Master/Flashlight-Master-BlackDiamond/Flashlight-Master-BlackDiamond/View Controllers/FlashlightViewController.swift
@@ -7,40 +7,67 @@
 //
 
 import UIKit
+// Need to import AVFoundation to gain access to the flashlight
+import AVFoundation
 
 class FlashlightViewController: UIViewController {
-
     
     //MARK: Outlets
     @IBOutlet weak var FlashlightToggleButton: UIButton!
     
     //MARK: Properties
     var isOn: Bool = false // Creates a boolean for us to track the flashlights state. default value is false - or "Off"
+    var gestureRecognizer: UISwipeGestureRecognizer?
+    var statusBarStyle: UIStatusBarStyle = .lightContent // With the backgound starting black I want the intial status bar to be white
     
     //MARK: ViewLifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        swipe(direction: isOn ? .right : .left)
+    }
+    
+    // MARK: Methods
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return statusBarStyle
+    }
+    // Flashlight Toggle Logic
+    func toggleTorch(on: Bool) {
+        guard let device = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+        if device.hasTorch && device.isTorchAvailable {
+            do {
+                try device.lockForConfiguration()
+                device.torchMode = on ? .on : .off
+                device.unlockForConfiguration()
+            } catch {
+                print("Torch could not be used \(device.systemPressureState)")
+            }
+        } else {
+            print("Torch is not available)")
+        }
+    }
+    // Swipe Gesture Logic
+    func swipe(direction: UISwipeGestureRecognizer.Direction) {
+        let swipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(FlashlightToggleButtonTapped(_:)))
+        swipeGestureRecognizer.direction = direction
+        view.addGestureRecognizer(swipeGestureRecognizer)
+        gestureRecognizer = swipeGestureRecognizer
     }
     
     // MARK: Actions
     @IBAction func FlashlightToggleButtonTapped(_ sender: Any) {
-        if isOn == true {
-            // When the button is tapped, and the "light" is on, let's turn it off.
-            self.view.backgroundColor = .black
-            // We want the text to change with the flashlights state.
-            FlashlightToggleButton.setTitle("Turn On", for: .normal)
-            FlashlightToggleButton.setTitleColor(.white, for: .normal)
-            
-            isOn = false // Set the boolean to the desired State.
-        } else {
-            // When the button is tapped and the "light" is off, let's turn it on.
-            self.view.backgroundColor = .white
-            // We want the text to change with the flashlights state.
-            FlashlightToggleButton.setTitle("Turn Off", for: .normal)
-            FlashlightToggleButton.setTitleColor(.black, for: .normal)
-            
-            isOn = true // Set the boolean to the desired State.
-        }
+        isOn = !isOn
+        // Ternary operators
+        view.backgroundColor = isOn ? .white : .black
+        FlashlightToggleButton.setTitle(isOn ? "Turn Off" : "Turn On", for: .normal)
+        FlashlightToggleButton.setTitleColor(isOn ? .black : .white, for: .normal)
+        statusBarStyle = isOn ? .default : .lightContent
+        // Update the statusbar color
+        setNeedsStatusBarAppearanceUpdate()
+        // Turn on the flash
+        toggleTorch(on: isOn)
+        // Handle the gesture
+        guard let gesture = gestureRecognizer else {return}
+        view.removeGestureRecognizer(gesture)
+        swipe(direction: isOn ? .right : .left)
     }
-    
 }
